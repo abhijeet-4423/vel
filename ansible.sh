b4f424aef33ef78740bc88b06fb8e59625941059
abhijeet-4423 <123581840+abhijeet-4423@users.noreply.github.com>
2023-03-07 18:48:00 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-sudo su service -c "ansible hello[0] -b -m copy -a 'src=index.html dest=/var/www/html/'"
+sudo su service -c "ansible hello[0] -b -m copy -a 'src=/mnt/23Q1/vel/index.html dest=/var/www/html/'"
